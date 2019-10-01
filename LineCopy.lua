6df8c29b100d2ebd67413b7fe6da13862a031945
nidcodes <53180204+nidcodes@users.noreply.github.com>
2019-10-01 23:48:29 +0800

    Update LineCopy.lua
    Updated to use standard functions

--- LineCopy.lua
+++ LineCopy.lua
@@ -1,37 +1,30 @@
+local parseCsv = require 'csv'
 function main(Data)
-
-  Data = Data:gsub('\r\n','\n')
-
-  tbl={}
-  cnt=0
-  start=1
-
-  for i = 1, #Data do
-    local c = Data:sub(i,i)
-
-    if(c =='\n') then
-      cnt=cnt+1
-      if(cnt==1) then
-        start=i+1
-      end
-    else
-      if(cnt ==1) then
-        if(c==',') then
-          table.insert(tbl,  Data:sub(start,i-1))
-          print(c)
-          start=i+1
-        end
-      end
-    end
-    print(start)
-    print(i)
-
-  end
-  table.insert(tbl,  Data:sub(start,#Data-1))
-
-  for i = 1,10000,1
-  do
-    print(i, tbl[2],tbl[3],tbl[4],tbl[5],tbl[6])
-  end
-
+   local Csv = parseCsv(Data, '|')
+   
+   print(Csv)
+   local Row1 = Csv[1]
+   print(Row1)
+   
+   local Row2 = table.remove(Csv)
+   print(Row2)
+   PerNum = Row2[3]
+   print(PerNum)
+   result={}
+   
+   PerNum=PerNum+0
+   
+   for i=1,10,1
+   do
+      print(PerNum)
+      t = {[1]='a',[2]='b',[4]='c'}
+      table.insert(t,3,PerNum)
+      table.insert(result,t)
+      
+      PerNum=PerNum+1
+   end
+   
+   table.insert(result,1,Row1)
+   print(result)
+   
 end
